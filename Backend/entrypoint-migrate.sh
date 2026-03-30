ace057ac4b2c9f1478050a2ea97bc8d08044e1cb
Alikhan <220103368@stu.sdu.edu.kz.com>
2026-03-30 22:09:02 +0500

    мигр

--- Backend/entrypoint-migrate.sh
+++ Backend/entrypoint-migrate.sh
@@ -2,5 +2,9 @@
 set -e
 
 echo "Waiting for database..."
+PGPASSWORD=$POSTGRES_PASSWORD psql -h postgres -U $POSTGRES_USER -tc "SELECT 1 FROM pg_database WHERE datname = '$POSTGRES_DB'" | grep -q 1 || \
+PGPASSWORD=$POSTGRES_PASSWORD psql -h postgres -U $POSTGRES_USER -c "CREATE DATABASE $POSTGRES_DB"
+
+echo "Running migrations..."
 alembic upgrade head
 echo "Migrations done."
